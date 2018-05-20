5a70a16dfd4b8fcad2f75c9868167f22fb48bc10
Ítalo Drago <cd_italo@hotmail.com>
2018-05-20 20:30:13 -0300

    Layout 0.0.4 - docker 0.7

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var app = require('./config/express')();
 
 const PORT = 8080;
-const HOST = '0.0.0.0';
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT, () =>  console.log(`Servidor rodando`));
