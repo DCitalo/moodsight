9d2d70139b0de77cb94c6d1faeec2a7819bc050b
Ítalo Drago <cd_italo@hotmail.com>
2018-05-24 23:47:55 -0300

    Layout 0.0.10 CORS - Heroku 1.0.0

--- config/express.js
+++ config/express.js
@@ -11,8 +11,26 @@ module.exports = function() {
     app.use(express.static('./app/public'))
     
     //middleware - BodyParser
-    app.use(bodyParser.urlencoded({extended : true}));
+    app.use(function (req, res, next) {
 
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', 'https://moodsight.herokuapp.com/');
+    
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+    
+        // Pass to next layer of middleware
+        next();
+    });
+    app.use(bodyParser.urlencoded({extended : true}));
+    
     load('routes',{cwd: 'app'}).into(app);
 
     return app;
