38015ccac36180df5a8208ffef3eff886d9d52f9
Ítalo Drago <cd_italo@hotmail.com>
2018-05-27 02:55:50 -0300

    Layout 0.0.21 CORS + footer - Heroku 1.0.0

--- config/express.js
+++ config/express.js
@@ -12,12 +12,15 @@ module.exports = function() {
     
     //middleware - BodyParser
     app.use(bodyParser.urlencoded({extended : true}));
-    app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    
+    app.use(function (req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', "*");
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        res.setHeader('Access-Control-Allow-Credentials', true);
         next();
-      });
-
+    });
+    
     load('routes',{cwd: 'app'}).into(app);
 
     return app;
