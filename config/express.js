d9a804cfe13e26b64307c5e1f96bf8458444d93d
Ítalo Drago <cd_italo@hotmail.com>
2018-05-27 02:23:00 -0300

    Layout 0.0.17 CORS - Heroku 1.0.0

--- config/express.js
+++ config/express.js
@@ -12,9 +12,9 @@ module.exports = function() {
     
     //middleware - BodyParser
     app.use(function (req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', 'https://www.moodsight.com.br/');
+        res.setHeader('Access-Control-Allow-Origin', "*");
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
         res.setHeader('Access-Control-Allow-Credentials', true);
         next();
     });
