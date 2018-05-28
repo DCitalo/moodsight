d238b4acd92b5b2aeb9f774b334b0c3419360e3a
Ítalo Drago <cd_italo@hotmail.com>
2018-05-28 14:32:10 -0300

    Layout 0.0.46 Pin image + css icons - Heroku 1.0.0

--- app/public/js/default.js
+++ app/public/js/default.js
@@ -27,20 +27,12 @@ function pintrestLogin(){
 				} else {
 					let	databoard = response.data;
 					console.log(databoard["0"].id);
-					PDK.request('/v1/boards/'+ databoard["0"].id +'/pins/', function (response) {
+					PDK.request('/v1/boards/'+ databoard["0"].id +'/pins/', {fields: 'id,note,link,url,image'} , function (response) {
 							if (!response || response.error) {
 							alert('Error occurred');
 							} else {
 								pins = pins.concat(response.data);
 								console.log(pins)
-								PDK.request('/pins/'+pins["0"].id, {fields: 'id,note,link,url,image'}, function (response) {
-									if (!response || response.error) {
-										console.log(response.error);	
-									}else{
-										var ThisPin = response.data;
-										console.log("3"+ThisPin.pin.images['136x136,236x,1200x']);
-									}
-								})
 								if (response.hasNext) {
 									response.next(); // this will recursively go to this same callback
 								}
