45e2c3efe33903b7763adc7854c4a1c5d4581a27
Ítalo Drago <cd_italo@hotmail.com>
2018-06-02 03:38:37 -0300

    Layout 0.0.61 teste firebase - Heroku 1.0.0

--- config/express.js
+++ config/express.js
@@ -12,7 +12,7 @@ module.exports = function() {
     
     //middleware - BodyParser
     app.use(bodyParser.urlencoded({extended : true}));
-    
+    app.use(bodyParser.json());
     app.use(function (req, res, next) {
         res.setHeader('Access-Control-Allow-Origin', "*");
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
