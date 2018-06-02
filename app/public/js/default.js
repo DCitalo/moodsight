1993fdd9b33409a9918a34c93a6e2b977e79ad4b
Ítalo Drago <cd_italo@hotmail.com>
2018-06-02 01:06:08 -0300

    Layout 0.0.57 - Heroku 1.0.0

--- app/public/js/default.js
+++ app/public/js/default.js
@@ -11,8 +11,55 @@ function pintrestLogin(){
 	        } else {
 	          console.log(response);
 			}
-			
-			//$.post('/salva', {obj});
+			let pins = [],
+                databoard = {},
+                datafirebase = [],
+                obj = [],
+		        dataMe = {};
+			PDK.request('/v1/me', {fields: 'id,username,first_name,last_name,image,bio'}, function (response) {
+			if (!response || response.error) {
+				console.log(response.error);
+			} else {
+                dataMe = response.data;
+                datafirebase.push({
+                    pessoal : dataMe
+                })
+			}
+			});
+			PDK.me('boards', function (response) {
+				if (!response || response.error) {
+					console.log(response.error);
+				} else {
+                    databoard = response.data;
+                    function ShowResults(value, index, ar) {
+                        PDK.request('/v1/boards/'+ databoard[index].id +'/pins/', {fields: 'board,id,note,link,url,image,color'} , function (response) {
+							if (!response || response.error) {
+							alert('Error occurred');
+							} else {
+							if (response.hasNext) {
+								response.next();
+                            }
+                            pins = response.data;
+                            for(var i = 0; i < pins.length; i++){
+                                datafirebase.push({
+                                    boardName: pins[i].board.name,
+                                    boardId: pins[i].board.id,
+                                    boardUrl: pins[i].url,
+                                    id: pins[i].id,
+                                    note: pins[i].note,
+                                    img: pins[i].image.original.url,
+                                    url: pins[i].url,
+                                    color: pins[i].color
+                                })
+                            }
+                        }
+                        })
+                    }
+					databoard.forEach(ShowResults)
+				}
+			});
+			console.log(datafirebase)
+			//$.post('/salva', {datafirebase});
 	    });
 	};
 	(function(d, s, id){
