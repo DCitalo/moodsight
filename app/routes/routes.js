0233f4245a274135549787bbfb90843cbdebb135
Ítalo Drago <cd_italo@hotmail.com>
2018-06-03 04:13:37 -0300

    Layout 0.0.73 home 90% + links - Heroku 1.0.0

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -7,39 +7,47 @@ module.exports = function(app) {
       });
     app.get("/",function(req, res) {
       res.render('home/index');
-    });
-    app.post("/salva", (req, res) => {
-      var db = admin.database();
-      var ref = db.ref("users");
-      var usersRef = ref.child(req.body.datafirebase.pessoal.id);
-      userRef.push({
-          nome: {
-            nome: req.body.datafirebase.pessoal.first_name,
-            sobrenome: req.body.datafirebase.pessoal.last_name
-          },
-          image_profile: {
-            url: req.body.datafirebase.pessoal.image["60x60"].url
-          }
+      app.post("/salva", (req, res) => {
+        var db = admin.database();
+        var ref = db.ref("users");
+        var usersRef = ref.child(req.body.datafirebase.pessoal.id);
+        userRef.push({
+            nome: {
+              nome: req.body.datafirebase.pessoal.first_name,
+              sobrenome: req.body.datafirebase.pessoal.last_name
+            },
+            image_profile: {
+              url: req.body.datafirebase.pessoal.image["60x60"].url
+            }
+        });
+        for(var i= 1; i < req.body.datafirebase.length; i++){
+          usersRef.push({
+            boardName: req.body.datafirebase[i].boardName,
+            boardId: req.body.datafirebase[i].boardId,
+            boardUrl: req.body.datafirebase[i].boardUrl,
+            id: req.body.datafirebase[i].id,
+            note: req.body.datafirebase[i].note,
+            img: req.body.datafirebase[i].img,
+            url: req.body.datafirebase[i].url,
+            color: req.body.datafirebase[i].color
+          })
+        }
+        // you have address available in req.body:
+        console.log(req.body.datafirebase);
+        // always send a response:
+        res.json({ ok: true });
       });
-      for(var i= 1; i < req.body.datafirebase.length; i++){
-        usersRef.push({
-          boardName: req.body.datafirebase[i].boardName,
-          boardId: req.body.datafirebase[i].boardId,
-          boardUrl: req.body.datafirebase[i].boardUrl,
-          id: req.body.datafirebase[i].id,
-          note: req.body.datafirebase[i].note,
-          img: req.body.datafirebase[i].img,
-          url: req.body.datafirebase[i].url,
-          color: req.body.datafirebase[i].color
-        })
-      }
-      // you have address available in req.body:
-      console.log(req.body.datafirebase);
-      // always send a response:
-      res.json({ ok: true });
     });
-
     app.get("/Dashboard",function(req, res) {
       res.render('Dashboard/index');
     });
+    app.get("/Sobre",function(req, res) {
+      res.render('Sobre/index');
+    });
+    app.get("/Ajuda",function(req, res) {
+      res.render('Ajuda/index');
+    });
+    app.get("/Contato",function(req, res) {
+      res.render('Contato/index');
+    });    
 }
