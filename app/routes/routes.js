17992f0b2bdfb1b724697efd48edd02eafb7c5f7
Ítalo Drago <cd_italo@hotmail.com>
2018-06-09 18:03:53 -0300

    Layout 0.1.11 - Firebase 1.0.7 - Heroku 1.0.0

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -18,12 +18,13 @@ module.exports = function(app) {
         userID = req.body.id;
         var db = admin.database();
         var ref = db.ref(userID);
+        var resultados = [];
         ref.on("boards", function(snapshot) {
-          var resultados = snapshot.val();
-          res.redirect('Dashboard/index', {data:resultados})
+          resultados = snapshot.val();
         }, function (errorObject) {
           console.log("The read failed: " + errorObject.code);
         });
+        res.redirect('Dashboard/index', {data:resultados})
       });
       app.post('/salva', (req, res) => {
         var db = admin.database();
