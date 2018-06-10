126c233b4b4a96bd9a452da09548d0b91bef13c8
Ítalo Drago <cd_italo@hotmail.com>
2018-06-10 07:01:02 -0300

    Layout 0.1.32 - Firebase 1.0.40 - Heroku 1.0.0

--- app/public/js/selecao-painel.js
+++ app/public/js/selecao-painel.js
@@ -1,16 +1,23 @@
 var data = JSON.parse(Cookies.get("basket-data")); 
 console.log(data)
-var NomeUsuario = data.user.nome.nome;
-$("#UsuarioNome").append(NomeUsuario);
-let numeroBoard = 1;
-$.each(data.boards,function(i, board){
-    let painel ="<div id="+numeroBoard+" class='painel-cont tamfont1'>\
-                    <span class='tamfont3 color-2 Upper bold font-1'>Painel "+numeroBoard+"</span>\
-                    <ul class='pin-car DF FW CSS'>"
-                        $.each(board.pins,function(i, pin){
-                            painel +="<li class='pin-img'><img src="+pin.img+"></li>"
-                        })
-        painel +="</ul></div>"
-    $("#Dashboard").append(painel);
-    numeroBoard++               
+$(document).ready(function(){
+    var NomeUsuario = data.user.nome.nome;
+    $("#UsuarioNome").append(NomeUsuario);
+    let numeroBoard = 1;
+    $.each(data.boards,function(i, board){
+        let painel ="<div id="+numeroBoard+" class='painel-cont tamfont1'>\
+                        <span class='tamfont3 color-2 Upper bold font-1'>Painel "+numeroBoard+"</span>\
+                        <ul class='pin-car DF FW CSS'>"
+                            $.each(board.pins,function(i, pin){
+                                painel +="<li class='pin-img'><img src="+pin.img+"></li>"
+                            })
+            painel +="</ul></div>"
+        $("#Dashboard").append(painel);
+        numeroBoard++               
+    })
+    $('.pin-car').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+      });
 })
