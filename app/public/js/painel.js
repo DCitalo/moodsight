79978bde9aff1563a99fbb212ed61fa8298ff787
Ítalo Drago <cd_italo@hotmail.com>
2018-06-10 08:41:08 -0300

    Layout 0.1.34 - Firebase 1.0.45 - Heroku 1.0.0

---
+++ app/public/js/painel.js
@@ -0,0 +1,16 @@
+var url = "http://colormind.io/api/";
+var data = {
+	model : "default",
+	input : [[44,43,44],[90,83,82],[57,46,44],[34,98,55],[89,32,45],[56,37,00],[43,31,16],[78,43,55]]
+}
+
+var http = new XMLHttpRequest();
+
+http.onreadystatechange = function() {
+	if(http.readyState == 4 && http.status == 200) {
+		var palette = JSON.parse(http.responseText).result;
+	}
+}
+
+http.open("POST", url, true);
+http.send(JSON.stringify(data));
