4878a9191d03bc0aa9a285790f82523d3047106b
Ítalo Drago <cd_italo@hotmail.com>
2018-06-16 18:34:45 -0300

    Layout 0.1.89 palete test - Firebase 1.1.3 - Heroku 1.0.0

--- app/public/js/lab.js
+++ app/public/js/lab.js
@@ -38,14 +38,14 @@ userRef.on('value', function (snapshot) {
 	}, 1000);*/
 	var url = "http://colormind.io/api/";
 	var snapshot = snapshot.val();
-	var snapshotColors;
+	var snapshotColors = [];
 	$.each(snapshot.pins, function (i, pin) {
 		var rgbColor = hexToRgb(pin.color)
 		snapshotColors += JSON.stringify(rgbColor.rgb);
 	})
 	var data = {
 		model: "default",
-		input: [snapshotColors]
+		input: JSON.parse(snapshotColors)
 	}
 	var http = new XMLHttpRequest();
 
