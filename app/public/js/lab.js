1360fbaed73d43442e44a9a1703202ba7a280d07
Ítalo Drago <cd_italo@hotmail.com>
2018-06-16 16:59:38 -0300

    Layout 0.1.63 palete test - Firebase 1.1.3 - Heroku 1.0.0

--- app/public/js/lab.js
+++ app/public/js/lab.js
@@ -14,28 +14,26 @@ function hexToRgb(hex) {
 		rgb: [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)]
 	} : null;
 }
+var url = "http://colormind.io/api/";
+var colors = {
+	model: "default",
+	input: ""
+}
 userRef.on('value', function (snapshot) {
 	var data = snapshot.val();
-	console.log(data)
-	var url = "http://colormind.io/api/";
-	var colors = {
-		model: "default",
-		input: ""
-	}
 	$.each(data.pins, function (i, pin) {
 		var rgbColor = hexToRgb(pin.color)
 		colors.input += '[' + rgbColor.rgb + ']';
 	})
 	console.log(colors)
-	var http = new XMLHttpRequest();
-	http.onreadystatechange = function () {
-		if (http.readyState == 4 && http.status == 200) {
-			var palette = JSON.parse(http.responseText).result;
-			console.log(palette)
-		}
-	}
-	http.open("POST", url, true);
-	http.send(JSON.stringify(colors));
 })
-
+var http = new XMLHttpRequest();
+http.onreadystatechange = function () {
+	if (http.readyState == 4 && http.status == 200) {
+		var palette = JSON.parse(http.responseText).result;
+		console.log(palette)
+	}
+}
+http.open("POST", url, true);
+http.send(JSON.stringify(colors));
 //'https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyAHcRZekasi - l8mC0Uj_eTweB_AM0NLpDc'
