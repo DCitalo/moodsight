5e9a7d07439ea5fa88ba5a8a5333776584d2e4c5
Ítalo Drago <cd_italo@hotmail.com>
2018-06-16 23:27:58 -0300

    Layout 0.2.1 palete test - Firebase 1.1.3 - Heroku 1.0.0

--- app/public/js/lab.js
+++ app/public/js/lab.js
@@ -3,16 +3,21 @@ var idBoard = $.cookie('idBoard');
 var boardRef = firebase.database().ref(id + "/boards/" + idBoard);
 var userRef = firebase.database().ref(id);
 function hexToRgb(hex) {
-	// Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
 	var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
 	hex = hex.replace(shorthandRegex, function (m, r, g, b) {
 		return r + r + g + g + b + b;
 	});
-
 	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
 	return result ? {
 		rgb: [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)]
 	} : null;
+}
+function componentToHex(c) {
+	var hex = c.toString(16);
+	return hex.length == 1 ? "0" + hex : hex;
+}
+function rgbToHex(r, g, b) {
+	return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 userRef.on('value', function (snapshot) {
 	var data = snapshot.val();
@@ -42,7 +47,7 @@ boardRef.on('value', function (snapshot) {
 			var p = 1;
 			$.each(palette, function (i, color) {
 				$('.c-bg-color-' + p).css("background-color", "rgb(" + color["0"] + "," + color["1"] + "," + color["2"] + ")")
-				console.log(color)
+				$('.c-text-' + p).append("#" + rgbToHex(color["0"] + "," + color["1"] + "," + color["2"]))
 				p++
 			})
 		}
