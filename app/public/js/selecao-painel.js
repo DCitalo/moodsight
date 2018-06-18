955a0ccf9b77c3b5f1549075f66a375e8c80dd7d
Ítalo Drago <cd_italo@hotmail.com>
2018-06-17 23:40:59 -0300

    Layout 0.3.3 acertos + salvar projeto - Firebase 1.1.7 - Heroku 1.0.0

--- app/public/js/selecao-painel.js
+++ app/public/js/selecao-painel.js
@@ -19,7 +19,7 @@ userRef.on('value', function (snapshot) {
             painel += "<li class='pin-img'><img src=" + pin.img + "></li>"
         })
         painel += "  </ul>\
-                        <button data-lab="+ boardId + " data-num=" + numeroBoard + "href='http://wwww.moodsight.com.br/Laboratorio' class='btn-start btn-round DF FW CSS color-4'><i class='icon-small icon-upload'></i><span class='start-text font-1 tamfont3 bold'>Iniciar</span></button>\
+                        <button data-lab="+ boardId + " data-num=" + numeroBoard + " class='btn-start btn-round DF FW CSS color-4'><i class='icon-small icon-upload'></i><span class='start-text font-1 tamfont3 bold'>Iniciar</span></button>\
                     </div>"
         $("#Dashboard").append(painel);
         numeroBoard++
